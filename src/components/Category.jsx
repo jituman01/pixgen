3949e95cc3a92198fef6f7026883b57b80935c86
jituman01 <naimulislamjitu585@gmail.com>
2026-05-07 23:32:10 +0600

    added category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { Button } from "@heroui/react";
+import Link from "next/link";
 
-const Category = () => {
+const Category = async () => {
+  const res = await fetch("https://pixgen-eta.vercel.app/category.json");
+  const categories = await res.json();
   return (
-    <div>
-      
+    <div className="mb-5 space-x-3">
+      {categories.map((category) => (
+        <Link
+          key={category.id}
+          href={`?category=${category.name.toLowerCase()}`}
+        >
+          {" "}
+          <Button variant="outline" size="sm">
+            {category.name}
+          </Button>
+        </Link>
+      ))}
     </div>
   );
 };
